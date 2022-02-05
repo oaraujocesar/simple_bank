9960566c4dca681a87fe6060110c4fb8c3b010fa
César O. Araújo <oaraujocesar@gmail.com>
2022-02-05 14:20:35 -0300

    test: :white_check_mark: Add unit test for get entry

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -3,12 +3,13 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/oaraujocesar/simple_bank/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateEntry(t *testing.T) {
+func createRandomEntry(t *testing.T) Entry {
 	account := createRandomAccount(t)
 
 	arg := CreateEntryParams{
@@ -26,4 +27,25 @@ func TestCreateEntry(t *testing.T) {
 
 	require.NotZero(t, entry.ID)
 	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t)
+}
+
+func TestGetEntry(t *testing.T) {
+	createdEntry := createRandomEntry(t)
+
+	entry, err := testQueries.GetEntry(context.Background(), createdEntry.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, createdEntry.AccountID, entry.AccountID)
+	require.Equal(t, createdEntry.Amount, entry.Amount)
+	require.Equal(t, createdEntry.ID, entry.ID)
+
+	require.WithinDuration(t, createdEntry.CreatedAt, entry.CreatedAt, time.Second)
 }
