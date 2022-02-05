065970d60f609f05a5955130a0a00f14349d5a79
César O. Araújo <oaraujocesar@gmail.com>
2022-02-04 21:52:59 -0300

    refactor: :recycle: Randozime Account creation params

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"testing"
 
+	"github.com/oaraujocesar/simple_bank/util"
 	"github.com/stretchr/testify/require"
 )
 
 func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
-		Owner: "César",
-		Balance: 100,
-		Currency: "USD",
+		Owner: util.RandomOwner(),
+		Balance: util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
