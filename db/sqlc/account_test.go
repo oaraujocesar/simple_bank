b4b1379f4f42323828bd60f912176bb461d3e5cf
César O. Araújo <oaraujocesar@gmail.com>
2022-02-04 22:02:57 -0300

    test: :white_check_mark: Add unit test for update account

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -49,3 +49,24 @@ func TestGetAccount(t *testing.T) {
 
 	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
 }
+
+func TestUpdateAccount(t *testing.T) {
+	account := createRandomAccount(t)
+
+	arg := UpdateAccountParams{
+		ID:      account.ID,
+		Balance: util.RandomMoney(),
+	}
+
+	updatedAccount, err := testQueries.UpdateAccount(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedAccount)
+
+	require.Equal(t, account.ID, updatedAccount.ID)
+	require.Equal(t, account.Owner, updatedAccount.Owner)
+	require.Equal(t, arg.Balance, updatedAccount.Balance)
+	require.Equal(t, account.Currency, updatedAccount.Currency)
+
+	require.WithinDuration(t, account.CreatedAt, updatedAccount.CreatedAt, time.Second)
+}
