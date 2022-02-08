01f4d3a0c5c46c2ff1407cc68011b0d093cf22f5
César O. Araújo <oaraujocesar@gmail.com>
2022-02-07 21:58:26 -0300

    feat: :sparkles: Update the accounts' balance on the transaction

--- db/query/account.sql
+++ db/query/account.sql
@@ -9,6 +9,13 @@ FROM accounts
 WHERE id = $1
 LIMIT 1;
 
+-- name: GetAccountForUpdate :one
+SELECT *
+FROM accounts
+WHERE id = $1
+LIMIT 1
+FOR NO KEY UPDATE;
+
 -- name: ListAccounts :many
 SELECT *
 FROM accounts
@@ -19,6 +26,12 @@ LIMIT $1 OFFSET $2;
 UPDATE accounts
 SET balance = $2
 WHERE id = $1
+RETURNING *;
+
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
 RETURNING *;
 
 -- name: DeleteAccount :exec
