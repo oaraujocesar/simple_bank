3da3b8fdf0f8db31fa41731056941b5fd5f7a46d
César O. Araújo <oaraujocesar@gmail.com>
2022-02-11 23:07:17 -0300

    feat: :sparkles: List accounts with pagination

--- api/account.go
+++ api/account.go
@@ -66,3 +66,33 @@ func (server *Server) getAccount(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, account)
 }
+
+type listAccountRequest struct {
+	Page    int32 `form:"page"  binding:"required,min=1"`
+	PerPage int32 `form:"perPage" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) listAccounts(ctx *gin.Context) {
+	var req listAccountRequest
+
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+
+		return
+	}
+
+	arg := db.ListAccountsParams{
+		Limit:  req.PerPage,
+		Offset: (req.Page - 1) * req.PerPage,
+	}
+
+	accounts, err := server.store.ListAccounts(ctx, arg)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
+}
--- api/server.go
+++ api/server.go
@@ -18,6 +18,7 @@ func NewServer(store *db.Store) *Server {
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
 
 	server.router = router
 
